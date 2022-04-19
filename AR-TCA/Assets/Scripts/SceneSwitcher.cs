e28bd713c83c92276d54d8fca0f80015b0ab73c7
Lyrics17 <57186088+Lyrics17@users.noreply.github.com>
2022-04-19 19:21:04 +0200

    Begin with unity frontend
    View Issue #8 for detail

---
+++ AR-TCA/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void runApp()
+    {
+        //Wird die Funktion durch einen Buttonklick aufgerufen wird zunächst der aktuelle buildIndex der Seite geholt und dann um 1 inkrementiert.
+        //Dadurch wird die nächste Scene aufgerufen
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void backToFirst()
+    {
+        SceneManager.LoadScene("LoginScene");
+    }
+    public void goToMain()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    //TODO: write function which leads everytime back to the main menu --> save buildIndex of main menu in var
+}
