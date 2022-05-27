f4075f657848caa76a1ca536f190367819e08e22
Lukas Vogrinc <lukasvogrinc@hotmail.de>
2022-05-27 15:17:55 +0200

    Added save QR code to mobile logic

--- AR-TCA/Assets/Scripts/SaveQRCode.cs
+++ AR-TCA/Assets/Scripts/SaveQRCode.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
-
 public class SaveQRCode : MonoBehaviour
 {
 
     public void saveQRCodeOnDevice(Texture2D qrCodeTexture, string filename)
     {
+        //Saves on PC
         //returns the qr texture and endcodes it to png 
         byte[] bytes = qrCodeTexture.EncodeToPNG();
 
-        File.WriteAllBytes(Application.dataPath + "/QRCodes/" + filename +  ".png", bytes);
+        File.WriteAllBytes(Application.dataPath + "/QRCodes/" + filename + ".png", bytes);
+
+        // TODO: Test on mobile
+        //Saves on mobile 
+        //source: https://github.com/yasirkula/UnityNativeGallery
+        // NativeGallery.SaveImageToGallery(qrCodeTexture, "Warhammer40K QRCodes", filename);
     }
 
 }
